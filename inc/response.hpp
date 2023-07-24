a4f276c3a22c450a66949bbede83625fade66315
gniper <gniper@proton.me>
2023-07-24 17:33:37 +0100

    .

---
+++ inc/response.hpp
@@ -0,0 +1,17 @@
+#ifndef RESPONSE_HPP
+#define RESPONSE_HPP
+
+typedef struct s_response {
+    std::string headers;
+    int         body_file;
+    bool        body;
+    bool        init;
+    bool        head_done;
+    int         read_bytes;
+    bool        cgi;
+    bool        cgi_response;
+    pid_t       cgi_pid;
+    std::string cgi_response_file_name;
+    bool        finish ;
+} response_t;
+#endif 
